376d404c20f684607487c034f5e20b6d49c4565e
newbee-dev <katesives@gmail.com>
2024-11-11 09:29:26 +0200

    Add ShoppingCart.js

---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,9 @@
+import React from "react";
+// // component that displays... will trigger reducer functions and also displays the state value
+// // To be imported in main parent component or App.js
+
+const ShoppingCart = () => {
+  return <div>ShoppingCart</div>;
+};
+
+export default ShoppingCart;
