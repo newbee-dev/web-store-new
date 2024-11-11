ecda9000471d10c1b3d5b0dacfc52b35c61fa7c1
newbee-dev <katesives@gmail.com>
2024-11-11 12:49:44 +0200

    Added imports, filepaths and initialised functionality Changes to Landing.js

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,28 @@
-import React from "react";
-// // component that displays... will trigger reducer functions and also displays the state value
 // // To be imported in main parent component or App.js
+// Landing.js component
+import React from "react";
+import "../styles/Landing.css";
+// import bootstrap styles functionality
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import link from router-dom to enable navigation
+import { Link } from "react-router-dom";
+// import logout action functionality from reducer
+import { logout } from "../redux/reducers/authSlice";
+// import reusable button component
+import ReusableButton from "../components/ReusableButton";
+// import useSelector and useDispatch to read and change state
+import { useSelector, useDispatch } from "react-redux";
 
 const Landing = () => {
+  // initialise useSelector and useDispatch
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  // dispatch logout action OnClick
+  const handleLogout = () => {
+    dispatch(logout());
+  };
   return <div>Landing</div>;
 };
 
