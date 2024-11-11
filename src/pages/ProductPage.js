7d38d52540ba3d5cd5f863562b06348a2a7e870a
newbee-dev <katesives@gmail.com>
2024-11-11 13:32:38 +0200

    Added imports and child components

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,6 +1,19 @@
-import React from "react";
 // // component that displays... will trigger reducer functions and also displays the state value
 // // To be imported in main parent component or App.js
+import React from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+// Import the action creator
+import { addToCart } from "../redux/reducers/cartSlice";
+// Import Link for navigation
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../styles/ProductPage.css";
+import ReusableButton from "../components/ReusableButton";
+import TotalPrice from "../components/TotalPrice";
 
 const ProductPage = () => {
   return <div>ProductPage</div>;
