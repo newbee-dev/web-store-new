e2da06622a936fa11a02d0c77a893503a4fe401d
newbee-dev <katesives@gmail.com>
2024-11-11 11:53:05 +0200

    Added changes

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Register from "../forms/Register";
 // // To be imported in main parent component or App.js
 
 const RegistrationPage = () => {
-  return <div>RegistrationPage</div>;
+  return (
+    <div>
+      <Register />
+    </div>
+  );
 };
 
 export default RegistrationPage;
