0bc5c0aab31a3b8e084225abcf0bc493a510adbb
newbee-dev <katesives@gmail.com>
2024-11-11 09:25:31 +0200

    RegistrationPage.js

---
+++ src/pages/RegistrationPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+// // To be imported in main parent component or App.js
+
+const RegistrationPage = () => {
+  return <div>RegistrationPage</div>;
+};
+
+export default RegistrationPage;
