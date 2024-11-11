5cde7c14cc6c8dcee0c6fc5fc6c6c835dbd84014
newbee-dev <katesives@gmail.com>
2024-11-11 15:09:27 +0200

    Added boostrap checkbox form to be rendered

--- src/ui-components/ShippingOptions.js
+++ src/ui-components/ShippingOptions.js
@@ -1,7 +1,36 @@
 import React from "react";
+// import bootstrap form to render
+import Form from "react-bootstrap/Form";
 
-const ShippingOptions = () => {
-  return <div>ShippingOptions</div>;
-};
+function ShippingOptions() {
+  return (
+    <Form>
+      <Form.Group controlId="formBasicRadio">
+        <Form.Label>Shipping Options</Form.Label>
+        <div>
+          <Form.Check
+            type="radio"
+            label="Standard Shipping"
+            name="shippingOption"
+            id="standardShipping"
+            defaultChecked
+          />
+          <Form.Check
+            type="radio"
+            label="Speedy Shipping"
+            name="shippingOption"
+            id="speedyShipping"
+          />
+          <Form.Check
+            type="radio"
+            label="Free Shipping"
+            name="shippingOption"
+            id="freeShipping"
+          />
+        </div>
+      </Form.Group>
+    </Form>
+  );
+}
 
 export default ShippingOptions;
