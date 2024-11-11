ac98a9359ac3cf8d3a615440b67b747875102025
newbee-dev <katesives@gmail.com>
2024-11-11 15:11:11 +0200

    imported ShippingOptions.js and included in return

--- src/pages/Shipping.js
+++ src/pages/Shipping.js
@@ -1,8 +1,13 @@
 import React from "react";
+import ShippingOptions from "../ui-components/ShippingOptions";
 // // To be imported in main parent component or App.js
 
 const Shipping = () => {
-  return <div>Shipping</div>;
+  return (
+    <div>
+      <ShippingOptions />
+    </div>
+  );
 };
 
 export default Shipping;
