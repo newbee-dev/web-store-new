ee59840023dfc3d804c0230ae0235a16074be92d
newbee-dev <katesives@gmail.com>
2024-11-11 11:15:30 +0200

    Added yup validation schema to Register.js

--- src/forms/Register.js
+++ src/forms/Register.js
@@ -29,4 +29,29 @@ const Register = () => {
       console.error("Registration error:", error);
     }
   };
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .required("First name is required")
+      .max(15, "First name must not exceed 15 characters"),
+    lastName: Yup.string()
+      .required("Last name is required")
+      .max(20, "Surname should not exceed 20 characters"),
+    username: Yup.string()
+      .required("Username is required")
+      .min(3, "Username must be at least 3 characters long")
+      .max(20, "Username must not exceed 20 characters"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters long")
+      .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+      .matches(/[0-9]/, "Password must contain at least one number")
+      .matches(
+        /[!@#$%^&*()]/,
+        "Password must contain at least one special character"
+      ),
+  });
 };
