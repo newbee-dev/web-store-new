5d182f06813cafe17b9139411913b2c3c09e766c
newbee-dev <katesives@gmail.com>
2024-11-13 19:46:37 +0200

    imported redux hooks and navigation, ensured user is logged in to access store, cart and shipping

--- src/header/NaviGate.js
+++ src/header/NaviGate.js
@@ -1,26 +1,25 @@
 import React from "react";
+// import react bootstrap components
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import Link from react router to enable navigation
+// import link for navigation
 import { Link } from "react-router-dom";
-// import ReusableButton from "../components/ReusableButton";
-// // In the component that displays we...
-// // import useSelector (retrieves state from store)
-// // import useDispatch (excecutes actions that modify state)
-// //Dispatch sends the message to the Redux store.
+// import redux hooks to read and change state
 import { useSelector, useDispatch } from "react-redux";
+// import relevent reducer action
 import { logout } from "../redux/reducers/authSlice";
+import { useNavigate } from "react-router-dom";
 
 function NaviGate() {
-  // useSelector function to get the state from the store
   const { user } = useSelector((state) => state.auth);
-  //   // dispatch function to execute reducer functions
-  //   // the store receives this action and passes it to the reducers, which then executes their code to modify the state
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(logout());
+    navigate("/");
   };
+
   return (
     <>
       <Navbar
@@ -33,35 +32,42 @@ function NaviGate() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Link className="nav-link my-link" to="/">
+            <Link
+              className="nav-link 
+ my-link"
+              to="/"
+            >
               Home
             </Link>
             <Link className="nav-link my-link" to="/about">
               About
             </Link>
-            <Link className="nav-link my-link" to="/products">
-              Store
-            </Link>
+            {user && (
+              <Link className="nav-link my-link" to="/products">
+                Store
+              </Link>
+            )}
           </Nav>
           <Nav>
             {user ? (
-              <button className="nav-link my-link" onClick={handleLogout}>
-                Logout
-              </button>
+              <>
+                <Link className="nav-link my-link" to="/cart">
+                  Cart
+                </Link>
+                <button className="nav-link my-link" onClick={handleLogout}>
+                  Logout
+                </button>
+              </>
             ) : (
               <>
                 <Link className="nav-link my-link" to="/registrationPage">
                   Register
                 </Link>
-
                 <Link className="nav-link my-link" to="/loginPage">
                   Login
                 </Link>
               </>
             )}
-            <Link className="nav-link my-link" to="/cart">
-              Cart
-            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
