f258507e44c1662983dd82ff48a66b638b67e626
newbee-dev <katesives@gmail.com>
2024-11-13 19:27:38 +0200

    imported & initialised useSelector, added styles to h4

--- src/pages/Shipping.js
+++ src/pages/Shipping.js
@@ -1,22 +1,35 @@
 import React from "react";
 import ShippingOptions from "../ui-components/ShippingOptions";
 import InfoPopup from "../ui-components/InfoPopup";
-import { Container, Row, Col } from "react-bootstrap";
-import "../styles/ShippingOptions.css";
+import { useSelector } from "react-redux";
 import ReusableButton from "../components/ReusableButton";
 
 const Shipping = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
+  const selectedShippingOption = useSelector(
+    (state) => state.cart.selectedShippingOption
+  );
+
+  const calculateGrandTotal = () => {
+    let shippingCost = 0;
+    if (selectedShippingOption === "standardShipping") {
+      shippingCost = 50;
+    } else if (selectedShippingOption === "speedyShipping") {
+      shippingCost = 120;
+    }
+    return totalPrice + shippingCost;
+  };
+
+  const grandTotal = calculateGrandTotal();
+
   return (
-    <div className="shipping-options">
-      <Container>
-        <Row>
-          <InfoPopup />
-          <ShippingOptions />
-        </Row>
-        <Col>
-          <ReusableButton>make payment</ReusableButton>
-        </Col>
-      </Container>
+    <div>
+      <InfoPopup />
+      <ShippingOptions />
+      <br />
+      <h4 className="small-text">Grand Total: R{grandTotal}</h4>
+      <ReusableButton>make payment</ReusableButton>
     </div>
   );
 };
