1d1c9797ee44b25204002af07fdc8aa8afa2e967
newbee-dev <katesives@gmail.com>
2024-11-13 15:06:19 +0200

    changes

--- src/pages/Shipping.js
+++ src/pages/Shipping.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ShippingOptions from "../ui-components/ShippingOptions";
 import InfoPopup from "../ui-components/InfoPopup";
+import { Container, Row, Col } from "react-bootstrap";
+import "../styles/ShippingOptions.css";
+import ReusableButton from "../components/ReusableButton";
 
 const Shipping = () => {
   return (
-    <div>
-      <InfoPopup />
-      <ShippingOptions />
+    <div className="shipping-options">
+      <Container>
+        <Row>
+          <InfoPopup />
+          <ShippingOptions />
+        </Row>
+        <Col>
+          <ReusableButton>make payment</ReusableButton>
+        </Col>
+      </Container>
     </div>
   );
 };
