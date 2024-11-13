35cee952b86a595791b4084ac13aed9da51b499e
newbee-dev <katesives@gmail.com>
2024-11-13 18:17:00 +0200

    Added dropdown and imports

--- src/ui-components/SizeDropdown.js
+++ src/ui-components/SizeDropdown.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
 
-const SizeDropdown = () => {
-  return <div>SizeDropdown</div>;
+const SizeDropdown = ({ onSelect }) => {
+  const [selectedSize, setSelectedSize] = useState("");
+
+  const handleSizeChange = (size) => {
+    setSelectedSize(size);
+    onSelect(size);
+  };
+
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="light" id="dropdown-basic">
+        Select Size
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => handleSizeChange("Small")}>
+          Small
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleSizeChange("Medium")}>
+          Medium
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleSizeChange("Large")}>
+          Large
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 };
 
 export default SizeDropdown;
